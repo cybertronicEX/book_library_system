6dcc8bd23ef3e00f2e8f49c24daa5119253b8883
cybertronicEX <101054616+it20606138@users.noreply.github.com>
2022-05-08 15:30:18 +0530

    first commit

---
+++ app/src/main/java/com/example/lmsnew/ActualMain.java
@@ -0,0 +1,61 @@
+package com.example.lmsnew;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.lmsnew.databinding.ActivityActualMainBinding;
+import com.example.lmsnew.databinding.ActivityMainBinding;
+
+public class ActualMain extends AppCompatActivity {
+
+    ActivityActualMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        binding = ActivityActualMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView2.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.nav_users:
+                    startActivity(new Intent(ActualMain.this,MainActivity.class));
+                    break;
+                case R.id.nav_settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+                case R.id.nav_books:
+                    replaceFragment(new BooksFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+    private void replaceActivity(Activity activity){
+        ActivityManager activityManager;
+
+    }
+}
